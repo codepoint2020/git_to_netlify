755cf80ce3e2c8872694321682b5737c5eb4dd3e
j-morales-code <jeromemorales86@live.com>
2024-02-09 06:30:24 +0800

    Second Commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
       <h1>Hello World</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut excepturi, labore possimus voluptatum ut temporibus porro saepe dignissimos. Soluta, sint!</p>
     </>
   );
 }
